8795ae551b1a6cf04d094e2cc2af842963033c8b
wxf-0415 <61744147+wxf-0415@users.noreply.github.com>
2021-11-13 13:45:30 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,25 @@
+﻿<?php 
+   $host        = "host=127.0.0.1"; 
+   $port        = "port=5432"; 
+   $dbname      = "dbname=testdb"; 
+   $credentials = "user=postgres password=pass123"; 
+ 
+   $db = pg_connect( "$host $port $dbname $credentials"  ); 
+   if(!$db){ 
+      echo "Error : Unable to open database\n"; 
+   } else { 
+      echo "Opened database successfully\n"; 
+   } 
+ 
+   $sql =<<<EOF 
+      INSERT INTO COMPANY (20194687,123456) 
+EOF; 
+ 
+   $ret = pg_query($db, $sql); 
+   if(!$ret){ 
+      echo pg_last_error($db); 
+   } else { 
+      echo "Records created successfully\n"; 
+   } 
+   pg_close($db); 
+?> 
---
+++ 2.php
@@ -0,0 +1,41 @@
+﻿<?php 
+   $host        = "host=127.0.0.1"; 
+   $port        = "port=5432"; 
+   $dbname      = "dbname=testdb"; 
+   $credentials = "user=postgres password=pass123"; 
+ 
+   $db = pg_connect( "$host $port $dbname $credentials"  ); 
+   if(!$db){ 
+      echo "Error : Unable to open database\n"; 
+   } else { 
+      echo "Opened database successfully\n"; 
+   } 
+   $sql =<<<EOF 
+      UPDATE COMPANY set SALARY = 25000.00 where ID=1; 
+EOF; 
+   $ret = pg_query($db, $sql); 
+   if(!$ret){ 
+      echo pg_last_error($db); 
+      exit; 
+   } else { 
+      echo "Record updated successfully\n"; 
+   } 
+    
+   $sql =<<<EOF 
+      SELECT * from COMPANY; 
+EOF; 
+ 
+   $ret = pg_query($db, $sql); 
+   if(!$ret){ 
+      echo pg_last_error($db); 
+      exit; 
+   }  
+   while($row = pg_fetch_row($ret)){ 
+      echo "ID = ". $row[0] . "\n"; 
+      echo "NAME = ". $row[1] ."\n"; 
+      echo "ADDRESS = ". $row[2] ."\n"; 
+      echo "SALARY =  ".$row[4] ."\n\n"; 
+   } 
+   echo "Operation done successfully\n"; 
+   pg_close($db); 
+?> 
